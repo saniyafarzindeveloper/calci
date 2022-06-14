2840c3bfe85d1ae9d18738aae4e8c2e7af124c71
farzinsaniya <saniyafarzin75@gmail.com>
2022-06-14 18:48:45 +0530

    created useEffect hook for button input

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import './App.css';
-import Output from './Output';
-import Keypad from './Keypad';
-
+import React, { useState , useEffect} from "react";
+import "./App.css";
+import Output from "./Output";
+import Keypad from "./Keypad";
 
 export default function App() {
-  return (
-    <div className='calculator-main-app' >
-    <Output />
+  const [inputValue, setInputValue] = useState("");
+  const [btnClick, setbtnClick] = useState("");
 
-    <div className='keypad'>
-    <Keypad  />
-    </div>
+  useEffect(() => {
+    if ("0123456789".includes(btnClick))
+      setInputValue((prev) => prev + btnClick);
+  }, [btnClick]);
+
+  return (
+    <div className="calculator-main-app">
+      <Output value={inputValue} />
+
+      <div className="keypad">
+        <Keypad setter={setbtnClick} />
+      </div>
     </div>
   );
 }
