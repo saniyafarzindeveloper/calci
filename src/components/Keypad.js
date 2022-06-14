17ed310c9ff71f7d4f350f40ee075402a8369675
farzinsaniya <saniyafarzin75@gmail.com>
2022-06-14 18:43:54 +0530

    created button mapper

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,52 +1,66 @@
-import React from 'react'
-import BackspaceOutlinedIcon from '@mui/icons-material/BackspaceOutlined';
-import {useState} from 'react';
+import React from "react";
+import BackspaceOutlinedIcon from "@mui/icons-material/BackspaceOutlined";
+import { useState } from "react";
+import btnList from "./btnMapper";
 
-export default function Keypad () {
-    const [res, setRes] = useState("");
+export default function Keypad({ setter }) {
+  const [res, setRes] = useState("");
 
   //implementing button functioning
-  const click = (c) =>{
+  const click = (c) => {
     setRes(res.concat(c.target.name));
-  }
+  };
 
   //implementing backspace
-  const del = () =>{
+  const del = () => {};
 
-  }
-
-    //implementing all clear (AC)
+  //implementing all clear (AC)
   const ac = () => {
-    setRes("")
-  }
+    setRes("");
+  };
 
   return (
-        <div className='keypad' >
-        <button id='trigo' name='sin' onClick={click}>sin</button>
-        <button id='trigo' name='cos' onClick={click}>cos</button>
-        <button id='trigo' name='tan' onClick={click}>tan</button>
-        <button id='trigo' name='()' onClick={click}>()</button>
-        <button className='ac' onClick={ac}>AC</button>
-        <button className='del' onClick={del} ><BackspaceOutlinedIcon /></button>
-        <button id='ari' name='/' onClick={click}>/</button>
-        <button id='ari' name='*' onClick={click}>*</button>
-        <button name='7' onClick={click} className='dig'>7</button>
-        <button name='8' onClick={click} className='dig'>8</button>
-        <button name='9' onClick={click} className='dig'>9</button>
-        <button id='ari' name='-' onClick={click}>-</button>
-        <button name='4' onClick={click} className='dig'>4</button>
-        <button name='5' onClick={click} className='dig'>5</button>
-        <button name='6' onClick={click} className='dig'>6</button>
-        <button id='ari' name='+' onClick={click}>+</button>
-        <button name='1' onClick={click} className='dig'>1</button>
-        <button name='2' onClick={click} className='dig'>2</button>
-        <button name='3' onClick={click} className='dig'>3</button>
-        <button className='equalto' onClick={click}>=</button>
-        <button name='0' onClick={click} className='dig' id='zero'>0</button>
-        <button name='.' onClick={click} className='dig'>.</button>
-       
-      </div>
+    <div className="keypad">
+      {btnList.map((btnObj, index) => (
+        <button
+          className={btnObj.type}
+          onClick={() => {
+            return setter(btnObj.content);
+          }}
+          key={index}>
+          {btnObj.content}
+        </button>
+      ))}
+    </div>
   );
+
+  // return (
+  //       <div className='keypad' >
+  //       <button id='trigo' name='sin' onClick={click}>sin</button>
+  //       <button id='trigo' name='cos' onClick={click}>cos</button>
+  //       <button id='trigo' name='tan' onClick={click}>tan</button>
+  //       <button id='trigo' name='()' onClick={click}>()</button>
+  //       <button className='ac' onClick={ac}>AC</button>
+  //       <button className='del' onClick={del} ><BackspaceOutlinedIcon /></button>
+  //       <button id='ari' name='/' onClick={click}>/</button>
+  //       <button id='ari' name='*' onClick={click}>*</button>
+  //       <button name='7' onClick={click} className='dig'>7</button>
+  //       <button name='8' onClick={click} className='dig'>8</button>
+  //       <button name='9' onClick={click} className='dig'>9</button>
+  //       <button id='ari' name='-' onClick={click}>-</button>
+  //       <button name='4' onClick={click} className='dig'>4</button>
+  //       <button name='5' onClick={click} className='dig'>5</button>
+  //       <button name='6' onClick={click} className='dig'>6</button>
+  //       <button id='ari' name='+' onClick={click}>+</button>
+  //       <button name='1' onClick={click} className='dig'>1</button>
+  //       <button name='2' onClick={click} className='dig'>2</button>
+  //       <button name='3' onClick={click} className='dig'>3</button>
+  //       <button className='equalto' onClick={click}>=</button>
+  //       <button name='0' onClick={click} className='dig' id='zero'>0</button>
+  //       <button name='.' onClick={click} className='dig'>.</button>
+       
+  //     </div>
+  // );
 }
 
 
---
+++ src/components/btnMapper.js
@@ -0,0 +1,93 @@
+const btnMapper = [
+    {
+        content: "sin",
+        type: "trigo"
+    },
+    {
+        content: "cos",
+        type: "trigo"
+    },
+    {
+        content: "tan",
+        type: "trigo"
+    },
+    {
+        content: "()",
+        type: "trigo"
+    },
+    {
+        content: "AC",
+        type: "all clear"
+    },
+    {
+        content: "Backspace",
+        type: "backspace"
+    },
+    {
+        content: "/",
+        type: "arithmetic"
+    },
+    {
+        content: "*",
+        type: "arithmetic"
+    },
+    {
+        content: "7",
+        type: "digit"
+    },
+    {
+        content: "8",
+        type: "digit"
+    },
+    {
+        content: "9",
+        type: "arithmetic"
+    },
+    {
+        content: "-",
+        type: "arithmetic"
+    },
+    {
+        content: "4",
+        type: "arithmetic"
+    },
+    {
+        content: "5",
+        type: "arithmetic"
+    },
+    {
+        content: "6",
+        type: "digit"
+    },
+    {
+        content: "+",
+        type: "digit"
+    },
+    {
+        content: "1",
+        type: "digit"
+    },
+    {
+        content: "2",
+        type: "digit"
+    },
+    {
+        content: "3",
+        type: "digit"
+    },
+    {
+        content: "=",
+        type: "equal"
+    },
+    {
+        content: "0",
+        type: "digit"
+    },
+    {
+        content: ".",
+        type: "period"
+    },
+  ];
+  
+  export default btnMapper;
+  
